f1a4ee7c7c5058f7cb3e782597b8cf314331c12e
endasil <jonas.stoor@recordunion.com>
2022-12-05 14:05:21 +0100

    Test where audio file properties are not recognized by atl but can see the length in windows explorer and goldwave. flac can not find any issues with the audio file.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using ATL;
+using System.IO;
+
+namespace TestAudio;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        using FileStream fs = new FileStream("effect.flac", FileMode.Open, FileAccess.Read,
+            FileShare.Read);
+        Track audioInfo = new(fs, "audio/flac");
+
+
+        Console.WriteLine("Audio length: " + audioInfo.Duration);
+    }
+}
