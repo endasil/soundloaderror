33b25cb92071c0380fc59a6dad25fb21dcaa0457
endasil <jonas.stoor@recordunion.com>
2022-12-05 14:13:14 +0100

    Add test for direct path too. Set fs.position to 0 before reading filestream.

--- Program.cs
+++ Program.cs
@@ -9,9 +9,14 @@ internal class Program
     {
         using FileStream fs = new FileStream("effect.flac", FileMode.Open, FileAccess.Read,
             FileShare.Read);
+
+        fs.Position = 0;
         Track audioInfo = new(fs, "audio/flac");
 
 
-        Console.WriteLine("Audio length: " + audioInfo.Duration);
+        Console.WriteLine("Audio length filestream: " + audioInfo.Duration);
+
+        Track audioInfo2 = new("effect.flac");
+        Console.WriteLine("Audio length direct path: " + audioInfo2.Duration);
     }
 }
